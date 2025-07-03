4fd54f26850c1497e900c29d948e86fb53f9819b
Amonre1975 <wojtekmaruniak@gmail.com>
2025-07-03 18:42:31 +0200

    dodanie plików

---
+++ button.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export const Button = ({ children, ...props }) => (
+  <button className="bg-blue-600 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700" {...props}>
+    {children}
+  </button>
+);
---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CubeQuiz from './components/CubeQuiz.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CubeQuiz />
+  </React.StrictMode>
+);
